8bbd235fa24186fa6d7ff1d91eba7affd82260e5
jseris <jseris@users.noreply.github.com>
2017-07-15 17:58:42 +0200

    Use external TOML config and start webserver
    * Config should be put in external TOML file; use --config flag
      to load it
    * Start a webserver to check current status of coin rates

--- coinmon.go
+++ coinmon.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/bluele/slack"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-var ccApiDomain string = "https://min-api.cryptocompare.com"
-
 var (
-	slackToken string = ""
-	slackApi          = slack.New(slackToken)
+	cfg        tomlCfg
+	slackApi   *slack.Slack
+	httpClient = http.Client{
+		Timeout: time.Second * 10,
+	}
 )
 
-var httpClient = http.Client{
-	Timeout: time.Second * 10,
+type tomlCfg struct {
+	CcApiDomain string
+	SlackToken  string
+	ServerPort  int
+	Coins       map[string]coin
 }
 
 type coin struct {
-	coinType     string
+	Symbol       string
 	Eur          float64 `json: "EUR"`
 	Usd          float64 `json: "USD"`
-	treshold     float64
-	pollInterval int
-	slackChannel string
+	Treshold     float64
+	PollInterval int
+	SlackChannel string
 }
 
 func getCoinValue(c *coin) {
-
-	res, err := httpClient.Get(fmt.Sprintf("%s/data/price?fsym=%s&tsyms=EUR,USD", ccApiDomain, c.coinType))
+	res, err := httpClient.Get(fmt.Sprintf("%s/data/price?fsym=%s&tsyms=EUR,USD", cfg.CcApiDomain, c.Symbol))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,24 +51,19 @@ func getCoinValue(c *coin) {
 }
 
 func pollCoinValue(coinCh chan<- *coin, c coin) {
-
 	for {
 		getCoinValue(&c)
 		coinCh <- &c
-		time.Sleep(time.Duration(c.pollInterval) * time.Second)
+		time.Sleep(time.Duration(c.PollInterval) * time.Second)
 	}
 }
 
 func sendSlackAlert(coinCh <-chan *coin) {
-
-	for {
-		c := <-coinCh
-
-		if c.Eur <= c.treshold {
-
-			coinValue := fmt.Sprintf("1 %s = %.2fEUR (%.2fUSD)", c.coinType, c.Eur, c.Usd)
+	for c := range coinCh {
+		if c.Eur <= c.Treshold {
+			coinValue := fmt.Sprintf("1 %s = %.2fEUR (%.2fUSD)", c.Symbol, c.Eur, c.Usd)
 			log.Print(coinValue)
-			err := slackApi.ChatPostMessage(c.slackChannel, coinValue, nil)
+			err := slackApi.ChatPostMessage(c.SlackChannel, coinValue, nil)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -69,32 +71,40 @@ func sendSlackAlert(coinCh <-chan *coin) {
 	}
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi! I have got the following information about your coins:\n\n")
+	for symbol, c := range cfg.Coins {
+		res, err := httpClient.Get(fmt.Sprintf("%s/data/price?fsym=%s&tsyms=EUR,USD", cfg.CcApiDomain, c.Symbol))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+		data, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		output := fmt.Sprintf("%s: %s\n", symbol, string(data))
+		fmt.Fprintf(w, output)
+	}
+}
+
 func main() {
 
-	coinCh := make(chan *coin)
-
-	coins := map[string]coin{
-		"ETH": coin{
-			coinType:     "ETH",
-			treshold:     250,
-			pollInterval: 500,
-			slackChannel: "general",
-		},
-		"BTC": coin{
-			coinType:     "BTC",
-			treshold:     2500,
-			pollInterval: 200,
-			slackChannel: "general",
-		},
+	cfgFile := flag.String("config", "coinmon.toml", "location to TOML config")
+	flag.Parse()
+	if _, err := toml.DecodeFile(*cfgFile, &cfg); err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	go sendSlackAlert(coinCh)
+	slackApi = slack.New(cfg.SlackToken)
+	coinCh := make(chan *coin)
 
-	for _, coin := range coins {
+	go sendSlackAlert(coinCh)
+	for _, coin := range cfg.Coins {
 		go pollCoinValue(coinCh, coin)
 	}
 
-	fmt.Println("\nDuring execution, press any key to close\n")
-	var input string
-	fmt.Scanln(&input)
+	http.HandleFunc("/status", handler)
+	http.ListenAndServe(":"+strconv.Itoa(cfg.ServerPort), nil)
 }
